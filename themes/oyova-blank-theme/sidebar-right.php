f80ba162dab1eeeec5b113adb676df00ca0e167b
Matt <matt.tippins@oyova.com>
2014-02-24 15:20:08 -0500

    sidebar changes

--- themes/oyova-blank-theme/sidebar-right.php
+++ themes/oyova-blank-theme/sidebar-right.php
@@ -6,11 +6,9 @@
  */
 ?>
 	<div id="secondary" class="widget-area left_sidebar" role="complementary">
-		<?php if ( ! dynamic_sidebar( 'sidebar-left' ) ) : ?>
-
+		<?php if ( ! dynamic_sidebar( 'sidebar-right' ) ) : ?>
 			<aside id="search" class="widget widget_search">
-				You should throw some widgets into the Left Sidebar.
+				You should throw some widgets into the Right Sidebar.
 			</aside>
-
 		<?php endif; // end sidebar widget area ?>
 	</div><!-- #secondary -->
