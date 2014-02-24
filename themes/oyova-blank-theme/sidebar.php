42f6621f174f157f08edddc1b600d4d1f1566762
Matt <matt.tippins@oyova.com>
2014-02-24 15:23:06 -0500

    removing extra character

--- themes/oyova-blank-theme/sidebar.php
+++ themes/oyova-blank-theme/sidebar.php
@@ -8,7 +8,7 @@
 	<div id="secondary" class="widget-area" role="complementary">
 		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
-			<<aside id="search" class="widget widget_search">
+			<aside id="search" class="widget widget_search">
 				You should throw some widgets into the Default Sidebar
 			</aside>
 
